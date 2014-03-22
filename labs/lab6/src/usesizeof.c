79afaa86eeba41dfea37a7263cbc93a7090c67fd
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2014-03-22 16:05:16 -0400

    Adding a more full-featured version of the usesizeof.c program.

--- labs/lab6/src/usesizeof.c
+++ labs/lab6/src/usesizeof.c
@@ -13,23 +13,26 @@ int main() {
     long double longDoubleVar;
 
     char charVar;
+    char* charPointerVar;
+
     _Bool boolVar;
 
-    printf("Size of short: %d bytes\n",sizeof(shortVar));
-    printf("Size of int: %d bytes\n",sizeof(intVar));
-    printf("Size of long: %d bytes\n",sizeof(longVar));
-    printf("Size of long long: %d bytes\n",sizeof(longLongVar));
+    printf("Size of short: %d bytes\n", sizeof(shortVar));
+    printf("Size of int: %d bytes\n", sizeof(intVar));
+    printf("Size of long: %d bytes\n", sizeof(longVar));
+    printf("Size of long long: %d bytes\n", sizeof(longLongVar));
     printf("\n");
 
-    printf("Size of float: %d bytes\n",sizeof(floatVar));
-    printf("Size of double: %d bytes\n",sizeof(doubleVar));
-    printf("Size of long double: %d bytes\n",sizeof(longDoubleVar));
+    printf("Size of float: %d bytes\n", sizeof(floatVar));
+    printf("Size of double: %d bytes\n", sizeof(doubleVar));
+    printf("Size of long double: %d bytes\n", sizeof(longDoubleVar));
     printf("\n");
 
-    printf("Size of _Bool: %d byte\n",sizeof(boolVar));
+    printf("Size of _Bool: %d byte\n", sizeof(boolVar));
     printf("\n");
 
-    printf("Size of char: %d byte\n",sizeof(charVar));
+    printf("Size of char: %d byte\n", sizeof(charVar));
+    printf("Size of char pointer: %d bytes\n", sizeof(charPointerVar));
     printf("\n");
     
     return 0;
